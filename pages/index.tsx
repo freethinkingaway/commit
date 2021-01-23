438be95b218762a939ecdead14b4a1a9d86e6678
freethinkingaway <39429106+freethinkingaway@users.noreply.github.com>
2021-01-22 16:34:48 -0800

    port index.js to tsx, clean up boilerplate, start with simple clean slate

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Commit</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.grid}>
+          <div className={styles.card}>
+            <div className={styles.title}>Commit</div>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+              Instantly deploy your Next.js site to a public URL with Vercel.
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
