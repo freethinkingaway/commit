5c807af84af2eb6e67021c3bb7e32bdd0a73cbe7
freethinkingaway <39429106+freethinkingaway@users.noreply.github.com>
2021-01-27 18:00:09 -0800

    initial Row integration into Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,17 @@
+import Link from "next/link";
 import styles from "../styles/Header.module.css";
+import Row from "./Row";
 
 export default function Header() {
-  return <div className={styles.container} />;
+  return (
+    <div className={styles.container}>
+      <Row>
+        <Row className={styles.link}>
+          <Link href=".">
+            <a>Commit</a>
+          </Link>
+        </Row>
+      </Row>
+    </div>
+  );
 }
