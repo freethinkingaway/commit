e808e83aa5cb670b3e5a72e6c776f8e6d1598fb5
freethinkingaway <39429106+freethinkingaway@users.noreply.github.com>
2021-01-28 17:35:00 -0800

    continue building out presentational Header component: integrate Card component, addtl styling, & testnet constants

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,44 @@
 import Link from "next/link";
+import { Text } from "rebass";
+import { ChainId } from "../common/constants";
 import styles from "../styles/Header.module.css";
 import Row from "./Row";
+import Card from "./Card";
+
+const SUPPORTED_TESTNETS: { [chainId in ChainId]?: string } = {
+  [ChainId.GÖRLI]: "Görli",
+  [ChainId.KOVAN]: "Kovan",
+};
 
 export default function Header() {
+  let chainId, account, userEthBalance;
   return (
     <div className={styles.container}>
       <Row>
         <Row className={styles.link}>
-          <Link href=".">
-            <a>Commit</a>
+          <Link href="." as="/commit">
+            <a className={styles.nav}>Commit</a>
           </Link>
         </Row>
       </Row>
+      <div className={styles.controls}>
+        <div className={styles.element}>
+          <span className={styles.small}>
+            {chainId && SUPPORTED_TESTNETS[chainId] && (
+              <Card className={styles.card} title={SUPPORTED_TESTNETS[chainId]}>
+                {SUPPORTED_TESTNETS[chainId]}
+              </Card>
+            )}
+          </span>
+          <div className={styles.account}>
+            {account && userEthBalance ? (
+              <Text className={styles.text}>
+                {/* {userEthBalance?.toSignificant(4)} ETH */}
+              </Text>
+            ) : null}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
