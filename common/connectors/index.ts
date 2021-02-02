1cb62162b4d9ff819a5f9e794e0f27bd1b9fcc4b
freethinkingaway <39429106+freethinkingaway@users.noreply.github.com>
2021-02-01 17:24:27 -0800

    lint & prettify

--- common/connectors/index.ts
+++ common/connectors/index.ts
@@ -1,31 +1,36 @@
-import { Web3Provider } from '@ethersproject/providers'
-import { InjectedConnector } from '@web3-react/injected-connector'
+import { Web3Provider } from "@ethersproject/providers";
+import { InjectedConnector } from "@web3-react/injected-connector";
 // import { WalletLinkConnector } from '@web3-react/walletlink-connector'
 
 // import { FortmaticConnector } from './Fortmatic'
-import { NetworkConnector } from './NetworkConnector'
+import { NetworkConnector } from "./NetworkConnector";
 
-const NETWORK_URL = process.env.NEXT_PUBLIC_NETWORK_URL
+const NETWORK_URL = process.env.NEXT_PUBLIC_NETWORK_URL;
 // const FORMATIC_KEY = process.env.NEXT_PUBLIC_FORTMATIC_KEY
 
-export const NETWORK_CHAIN_ID: number = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID ?? '1')
+export const NETWORK_CHAIN_ID: number = parseInt(
+  process.env.NEXT_PUBLIC_CHAIN_ID ?? "1"
+);
 
-if (typeof NETWORK_URL === 'undefined') {
-  throw new Error(`NEXT_PUBLIC_NETWORK_URL must be a defined environment variable`)
+if (typeof NETWORK_URL === "undefined") {
+  throw new Error(
+    `NEXT_PUBLIC_NETWORK_URL must be a defined environment variable`
+  );
 }
 
 export const network = new NetworkConnector({
-  urls: { [NETWORK_CHAIN_ID]: NETWORK_URL }
-})
+  urls: { [NETWORK_CHAIN_ID]: NETWORK_URL },
+});
 
-let networkLibrary: Web3Provider | undefined
+let networkLibrary: Web3Provider | undefined;
 export function getNetworkLibrary(): Web3Provider {
-  return (networkLibrary = networkLibrary ?? new Web3Provider(network.provider as any))
+  return (networkLibrary =
+    networkLibrary ?? new Web3Provider(network.provider as any));
 }
 
- export const injected = new InjectedConnector({
-   supportedChainIds: [1, 5, 42]
- })
+export const injected = new InjectedConnector({
+  supportedChainIds: [1, 5, 42],
+});
 
 // // mainnet only
 // export const fortmatic = new FortmaticConnector({
