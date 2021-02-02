6d1ac7b7e8490f6a40e7e9078acda0021841371f
freethinkingaway <39429106+freethinkingaway@users.noreply.github.com>
2021-02-01 17:23:40 -0800

    instantiate Web3React context (Provider, ProviderNetwork, ReactManager, etc) and inject from top-level

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import { AppProps } from "next/app";
+import { createWeb3ReactRoot, Web3ReactProvider } from "@web3-react/core";
+import Web3ReactManager from "../components/Web3ReactManager";
+import { NETWORK_CONTEXT } from "../common/constants";
+import { getWeb3Library } from "../utils";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const Web3ProviderNetwork = createWeb3ReactRoot(NETWORK_CONTEXT);
+
+if (typeof window !== "undefined" && !!window.ethereum) {
+  window.ethereum.autoRefreshOnNetworkChange = false;
 }
 
-export default MyApp;
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Web3ReactProvider getLibrary={getWeb3Library}>
+      <Web3ProviderNetwork getLibrary={getWeb3Library}>
+        <Web3ReactManager>
+          <Component {...pageProps} />
+        </Web3ReactManager>
+      </Web3ProviderNetwork>
+    </Web3ReactProvider>
+  );
+}
+
+export default App;
