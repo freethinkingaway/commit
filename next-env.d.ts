601625c1482e39e65ca9044aee9d4b66ae43ff64
freethinkingaway <39429106+freethinkingaway@users.noreply.github.com>
2021-02-01 17:16:15 -0800

    add initial browser window.ethereum & window.web3 typings

---
+++ next-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+ 
+interface Window {
+    ethereum?: {
+      isMetaMask?: true
+      on?: (...args: any[]) => void
+      removeListener?: (...args: any[]) => void
+      autoRefreshOnNetworkChange?: boolean
+    }
+    web3?: Record<string, unknown>
+  }
